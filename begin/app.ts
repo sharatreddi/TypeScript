7bb95a6437334341ed6e1b61e49334b281c10587
Sharat Reddi <110130577+Cherry0777@users.noreply.github.com>
2023-06-05 19:32:57 +0530

    local add

--- begin/app.ts
+++ begin/app.ts
@@ -3,10 +3,31 @@
 const person: { //this is the object type inferred by typescript
     name : string; //we dont have key va;ue pairs, we have key-type pairs
     age : number;
+    hobbies : string[]; // <---------- Array
+    role : [number, string] //  <---------- Tuple
 } = {
-    //const person = { generally, this is the good practice i.e.; we wrtie objects in ts like the same as we write in js
     name : 'Madmax',
-    age : 16
+    age : 16,
+    //                                  Array 
+    hobbies: ['Football', 'Training', 'Tech'], 
+    //                                  Tuple
+    role : [7, 'left wing']
 };
+
+// --------------------------------------about arrays----------------------------
+// let favoriteActivities: any[];
+// favoriteActivities = ['football', 7] we can actlly insert any kind of values
+console.log(person.name);
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
+  }
+
+// --------------------------------------about tuples----------------------------
+ //in tuples, we can define anything in them 
+ // person.role.push('admin'); <---- this works, unfortunately, typescript cant identify these push errors
+// person.role[1] = 10; <------ this does not work as the 1st element in the tuples is string
+
+// person.role = [0, 'admin', 'user']; <----- this wont work either, as we defined only for length 2 
 
 console.log(person.name);
